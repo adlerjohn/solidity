789872f610edf84f64d90d04d6f255acb177cb60
chriseth <chris@ethereum.org>
2021-08-09 17:36:19 +0200

    Formalization of SIGNEXTEND and rule proofs

---
+++ test/formal/signextend_shift.py
@@ -0,0 +1,43 @@
+from rule import Rule
+from opcodes import *
+
+"""
+Rule:
+1) SHL(A, SIGNEXTEND(B, X)) -> SIGNEXTEND(A / 8 + B, SHL(A, X))
+given A % 8 == 0 AND A <= 256 && B <= 32
+
+2) SIGNEXTEND(A, SHR(B, X)) -> SAR(B, X)
+given
+    B % 8 == 0 AND
+    A <= WordSize
+    B <= wordSize
+    (WordSize - B) / 8 == A + 1
+"""
+
+n_bits = 128
+
+# Input vars
+X = BitVec('X', n_bits)
+Y = BitVec('Y', n_bits)
+A = BitVec('A', n_bits)
+B = BitVec('B', n_bits)
+
+rule1 = Rule()
+rule1.require(A % 8 == 0)
+rule1.require(ULE(A, 256))
+rule1.require(ULE(B, 32))
+rule1.check(
+    SHL(A, SIGNEXTEND(B, X)),
+    SIGNEXTEND(A / 8 + B, SHL(A, X))
+)
+
+rule2 = Rule()
+rule2.require(B % 8 == 0)
+rule2.require(ULE(A, n_bits))
+rule2.require(ULE(B, n_bits))
+rule2.require((BitVecVal(n_bits, n_bits) - B) / 8 == A + 1)
+rule2.check(
+    SIGNEXTEND(A, SHR(B, X)),
+    SAR(B, X)
+)
+
